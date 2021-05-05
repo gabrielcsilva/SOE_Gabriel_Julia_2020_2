2e407650bc62dec17db4f36ae22e502a44a0f922
gabrielcsilva <csilvagabriel@hotmail.com>
2021-05-05 08:49:08 -0300

    abertura da branch de testes com opencv

--- Código-fonte/C++/carCounter.cpp
+++ Código-fonte/C++/carCounter.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/bgsegm.hpp>
+#include <opencv2/tracking.hpp>
 #include <stdio.h>
 #include <iostream>
 #include <list>
@@ -8,17 +9,7 @@
 using namespace cv;
 using namespace std;
 
-//Variaveis globais
 int carros = 0;
-int caminhoes = 0;
-
-int largura_min = 80;
-int altura_min = 80;
-int largura_max = 280;
-int altura_max = 280;
-int offset = 6;
-int pos_linha = 550;
-std::vector< vector<int> > detec;
 
 std::vector<int> pegaCentro(int x, int y, int largura, int altura)
 {
@@ -52,21 +43,9 @@ void setInfo(vector< vector<int> > detec, Mat frame, int pos_linha, int offset)
 	}
     }	
 
-int main(int argc, char** argv)
+Mat createBG(Mat frame, Ptr<BackgroundSubtractor> subtracao)
 {
-	
-VideoCapture cap("../../Videos/highway_3.mp4"); // open the video file
-if(!cap.isOpened())  // check if we succeeded
-    return -1;
-
-//create Background Subtractor objects
-Ptr<BackgroundSubtractor> subtracao;
-subtracao = bgsegm::createBackgroundSubtractorMOG();
-
-for(;;)
-{
-    Mat frame, grey, blur, img_sub, img_dilat, img_dilat_2, img_open, img_close;
-    cap >> frame; // get a new frame from camera
+    Mat grey, blur, img_sub, img_dilat, img_dilat_2, img_open, img_close;
     
     cvtColor(frame, grey, COLOR_BGR2GRAY);  //Pega o frame e transforma para preto e branco
     GaussianBlur(grey, blur, Size(3, 3), 5);  //Faz um blur para tentar remover as imperfeições da imagem
@@ -87,39 +66,85 @@ for(;;)
    
     //Closing
     Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5)); 
-    morphologyEx(img_dilat_2, img_close, MORPH_CLOSE, kernel);    
+    morphologyEx(img_dilat_2, img_close, MORPH_CLOSE, kernel);   
      
-    std::vector<std::vector<cv::Point> > contorno;
-     
-    findContours(img_close, contorno, RETR_TREE, CHAIN_APPROX_SIMPLE);
-    line(frame, Point(25, pos_linha), Point(1200, pos_linha), (255, 127, 0), 3);
-    
-    for (int i=0; i < contorno.size(); i++)
-    {
-	cv::Rect boundRect = boundingRect(contorno[i]);
-	
-	int x = boundRect.x;
-	int y = boundRect.y;
-	int w = boundRect.width;
-	int h = boundRect.height;
-	
-	bool validar_contorno = (w >= largura_min) && (h >= altura_min) && (w < largura_max) && (h < altura_max);
-	if (!validar_contorno)
-		continue;
+    return img_close;
+    }
 
-	rectangle(frame, Point(x, y), Point(x + w, y + h), (0, 255, 0), 2);
-	
-	std::vector<int> centro = pegaCentro(x, y, w, h);
-	detec.push_back(centro);
-	
-	circle(frame, Point(centro[0], centro[1]), 4, (0, 0, 255), -1);
+int main(int argc, char** argv){
+    
+    int largura_min = 80;
+    int altura_min = 80;
+    int largura_max = 280;
+    int altura_max = 280;
+    int offset = 6;
+    int pos_linha = 550;
+
+    //std::vector< vector<int> > detec;
+    
+    Ptr<Tracker> tracker = TrackerKCF::create();
+    vector<Ptr<Tracker>> trackers;
+    
+    vector<Rect> tempPos;
+    Rect roi;
+
+    VideoCapture cap("../../Videos/highway_3.mp4"); // open the video file
+    if(!cap.isOpened())  // check if we succeeded
+    return -1;
+
+    //create Background Subtractor objects
+    Ptr<BackgroundSubtractor> subtracao;
+    subtracao = bgsegm::createBackgroundSubtractorMOG();
+
+    for(;;){
+	Mat frame, img_close;
+	cap >> frame;
+
+	img_close = createBG(frame, subtracao);
+
+	//Obj detection
+	std::vector<std::vector<cv::Point> > contorno;
+	findContours(img_close, contorno, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+	for (int i=0; i < contorno.size(); i++){
+	    //Determine valid contour
+	    cv::Rect boundRect = boundingRect(contorno[i]);
+
+	    int x = boundRect.x;
+	    int y = boundRect.y;
+	    int w = boundRect.width;
+	    int h = boundRect.height;
+
+	    bool validar_contorno = (w >= largura_min) && (h >= altura_min) && (w < largura_max) && (h < altura_max);
+	    if (!validar_contorno)
+		continue;
+		
+	    tempPos.push_back(boundRect);
+
+	    //std::vector<int> centro = pegaCentro(x, y, w, h);
+	    //detec.push_back(centro);
+
+	    //circle(frame, Point(centro[0], centro[1]), 4, (0, 0, 255), -1);
+	    }
+
+	bool isTrack = false;
+	for (int i = 0; i < tempPos.size(); i++){
+	    for(int j = 0; j < trackers.size(); j++){
+		trackers[j]->update(frame, roi);
+		if(roi == tempPos[i])
+		    isTrack = true;
+		}
+	    if(!isTrack)
+		tracker->init(frame, tempPos[i]);
+		trackers.push_back(tracker);
+	    rectangle(frame, tempPos[i], (0, 255, 0), 2);
+	    }
+
+	line(frame, Point(25, pos_linha), Point(1200, pos_linha), (255, 127, 0), 3);
+	//setInfo(detec, frame, pos_linha, offset);
+	imshow("Video", frame);
+	if(waitKey(30) >= 0) break;
+	tempPos.clear();
 	}
-    setInfo(detec, frame, pos_linha, offset);
-    imshow("Video", frame);
-    //cout <<  "Carros detectados" << carros;
-    //imshow("Video", img_close);
-    if(waitKey(30) >= 0) break;
-    detec.clear();
-}
-return 0;
-}
+    return 0;
+    }
