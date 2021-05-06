fb6be10a155bf460b31c1fc7907b42ab3204d72e
gabrielcsilva <csilvagabriel@hotmail.com>
2021-05-06 17:10:39 -0300

    Versão funcional da detecção com tracking

--- Código-fonte/C++/carCounter.cpp
+++ Código-fonte/C++/carCounter.cpp
@@ -6,6 +6,8 @@
 #include <list>
 #include <iterator>
 #include <unistd.h>
+#include <vector>
+#include <list>
 using namespace cv;
 using namespace std;
 
@@ -22,26 +24,6 @@ std::vector<int> pegaCentro(int x, int y, int largura, int altura)
     
     return centro;
     }
-
-	
-void setInfo(vector< vector<int> > detec, Mat frame, int pos_linha, int offset)
-{
-    for(int i=0; i < detec.size(); i++)
-    {
-	 if(pos_linha + offset > detec[i][1])
-	{
-	    if(detec[i][1] > pos_linha - offset)
-	    {
-		cout << "X" << i << ", ";
-		cout << "Eixo X" << detec[i][0] << ", ";
-		cout << "Eixo Y" << detec[i][1] << endl;
-		carros++;
-		line(frame, Point(25, pos_linha), Point(1200, pos_linha), (0, 127, 255), 3);
-		cout << "Carros detectados ate o momento:" << carros << endl;
-		}
-	    }
-	}
-    }	
 
 Mat createBG(Mat frame, Ptr<BackgroundSubtractor> subtracao)
 {
@@ -71,22 +53,31 @@ Mat createBG(Mat frame, Ptr<BackgroundSubtractor> subtracao)
     return img_close;
     }
 
+bool pointInRect(int x, int y, int w, int h, int cx, int cy){
+    int x1 = cx;
+    int y1 = cy;
+    if (x < x1 && x1 < x+w)
+        if (y < y1 && y1 < y+h)
+            return true;
+    else
+        return false;  
+    }
+
+
 int main(int argc, char** argv){
     
     int largura_min = 80;
     int altura_min = 80;
-    int largura_max = 280;
-    int altura_max = 280;
-    int offset = 6;
+    int offset = 12;
     int pos_linha = 550;
-
-    //std::vector< vector<int> > detec;
     
-    Ptr<Tracker> tracker = TrackerKCF::create();
+    int frameCounter = 0;
+    Rect boundRect;
+    
+    
     vector<Ptr<Tracker>> trackers;
-    
-    vector<Rect> tempPos;
-    Rect roi;
+    vector<Rect> roi;
+    vector< bool > flag;
 
     VideoCapture cap("../../Videos/highway_3.mp4"); // open the video file
     if(!cap.isOpened())  // check if we succeeded
@@ -100,51 +91,98 @@ int main(int argc, char** argv){
 	Mat frame, img_close;
 	cap >> frame;
 
-	img_close = createBG(frame, subtracao);
+	if(frameCounter % 2 == 0){
+	    //cout << "Detect";
+	    //Obj detection
+	    img_close = createBG(frame, subtracao);
+	    
+	    std::vector<std::vector<cv::Point> > contorno;
+	    findContours(img_close, contorno, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-	//Obj detection
-	std::vector<std::vector<cv::Point> > contorno;
-	findContours(img_close, contorno, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	    for (int i=0; i < contorno.size(); i++){
+		//cout << "-" ;
+		//Determine valid contour
+		boundRect = boundingRect(contorno[i]);
 
-	for (int i=0; i < contorno.size(); i++){
-	    //Determine valid contour
-	    cv::Rect boundRect = boundingRect(contorno[i]);
+		int x = boundRect.x;
+		int y = boundRect.y;
+		int w = boundRect.width;
+		int h = boundRect.height;
 
-	    int x = boundRect.x;
-	    int y = boundRect.y;
-	    int w = boundRect.width;
-	    int h = boundRect.height;
-
-	    bool validar_contorno = (w >= largura_min) && (h >= altura_min) && (w < largura_max) && (h < altura_max);
-	    if (!validar_contorno)
-		continue;
+		bool validar_contorno = (w >= largura_min) && (h >= altura_min);
+		if (!validar_contorno)
+		    continue;
 		
-	    tempPos.push_back(boundRect);
-
-	    //std::vector<int> centro = pegaCentro(x, y, w, h);
-	    //detec.push_back(centro);
-
-	    //circle(frame, Point(centro[0], centro[1]), 4, (0, 0, 255), -1);
-	    }
-
-	bool isTrack = false;
-	for (int i = 0; i < tempPos.size(); i++){
-	    for(int j = 0; j < trackers.size(); j++){
-		trackers[j]->update(frame, roi);
-		if(roi == tempPos[i])
-		    isTrack = true;
+		bool isTracked = false;
+		std::vector<int> centro = pegaCentro(x, y, w, h);
+		
+		//cout << ".";
+		
+		for(int i = 0; i < trackers.size(); i++){
+		    //cout << "!";
+		    Rect pos; 
+		    trackers[i]->update(frame, pos);
+		    int x = pos.x;
+		    int y = pos.y;
+		    int w = pos.width;
+		    int h = pos.height;
+		    std::vector<int> centroTracker = pegaCentro(x, y, w, h);
+		    isTracked = pointInRect(x, y, w, h, centroTracker[0], centroTracker[1]);
+		    }
+		
+		if (isTracked == false){
+		    //cout << "?";
+		    Ptr<Tracker> tracker = TrackerKCF::create();
+		    tracker->init(frame, boundRect);
+		    trackers.push_back(tracker);
+		    roi.push_back(boundRect);
+		    flag.push_back(isTracked);
+		    }
 		}
-	    if(!isTrack)
-		tracker->init(frame, tempPos[i]);
-		trackers.push_back(tracker);
-	    rectangle(frame, tempPos[i], (0, 255, 0), 2);
+	    }
+	else{
+	    //cout << "Count - size: ";
+	    //cout << trackers.size() << " ";
+	    vector<int> it;
+	    for(int i = 0; i < trackers.size(); i++)
+	    {
+		//cout << "|";
+		trackers[i]->update(frame, roi[i]);
+		if(roi[i].y+roi[i].height > pos_linha && flag[i] == false){
+		    carros++;
+		    flag[i] = true;
+		    line(frame, Point(25, pos_linha), Point(1200, pos_linha), (0, 127, 255), 6);
+		    cout << "Carros detectados ate o momento:" << carros << endl;
+		    }
+		else if (roi[i].y < pos_linha && flag[i] == false){
+		    carros++;
+		    cout << "Carros detectados ate o momento:" << carros << endl;
+		    line(frame, Point(25, pos_linha), Point(1200, pos_linha), (0, 127, 255), 6);
+		    }
+		if(roi[i].y+roi[i].height > 600 || roi[i].y < 200){
+		    it.push_back(i);
+		    }
+		}
+	    for(int i = 0; i < it.size(); i++){
+		trackers.erase(trackers.begin()+it[i]);
+		trackers.shrink_to_fit();
+		roi.erase(roi.begin()+it[i]);
+		roi.shrink_to_fit();
+		flag.erase(flag.begin()+it[i]);
+		flag.shrink_to_fit();
+		}
 	    }
 
-	line(frame, Point(25, pos_linha), Point(1200, pos_linha), (255, 127, 0), 3);
-	//setInfo(detec, frame, pos_linha, offset);
+	for(int i = 0; i < trackers.size(); i++){
+	    rectangle(frame, roi[i], Scalar( 0, 255, 0 ), 2);
+	    vector<int> center = pegaCentro(roi[i].x, roi[i].y, roi[i].width, roi[i].height);
+	    circle(frame, Point(center[0], center[1]), 4, (0, 0, 255), -1);
+	    }
+
+	line(frame, Point(25, pos_linha), Point(1200, pos_linha), (255, 127, 0), 6);
 	imshow("Video", frame);
+	frameCounter++;
 	if(waitKey(30) >= 0) break;
-	tempPos.clear();
 	}
     return 0;
     }
